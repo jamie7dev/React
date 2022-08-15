f13618cbf83b586b42a1a0dcbb27961832478b1c
Chaewon Yoon(Jamie) <104494969+jamie7dev@users.noreply.github.com>
2022-08-15 23:10:23 +0900

    Create Counter.js

---
+++ Counter.js
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+
+function App() {
+  const [number, setNumber] = useState(0);
+  
+
+  return(
+    <div>
+      <div>{number}</div>
+      <button
+       onClick={() => {
+          setNumber(number + 1);
+       }}>
+        +
+      </button>
+      <button
+       onClick={() => {
+          setNumber(number - 1);
+       }}>
+        -
+      </button>
+    </div>
+
+  );
+}
+
+export default App;
